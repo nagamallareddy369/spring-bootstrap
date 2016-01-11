8ea80c60412c030132daf43c2213816ab68eb393
yi.zeng <yi.zeng@msn.com>
2016-01-11 17:20:19 +0800

    update test

--- src/main/java/com/boostrap/sample/ToncentServlet.java
+++ src/main/java/com/boostrap/sample/ToncentServlet.java
@@ -4,15 +4,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.qq.weixin.mp.aes.AesException;
 import com.qq.weixin.mp.aes.WXBizMsgCrypt;
 import com.riversoft.weixin.common.decrypt.MessageDecryption;
+import com.riversoft.weixin.common.event.ClickEvent;
+import com.riversoft.weixin.common.message.MsgType;
 import com.riversoft.weixin.common.message.XmlMessageHeader;
 import com.riversoft.weixin.common.message.xml.TextXmlMessage;
 import com.riversoft.weixin.common.util.XmlObjectMapper;
-import com.riversoft.weixin.qy.base.AgentSetting;
-import com.riversoft.weixin.qy.base.DefaultSettings;
+import com.riversoft.weixin.qy.event.QyClickEvent;
 import com.riversoft.weixin.qy.message.QyXmlMessages;
+import com.riversoft.weixin.qy.message.json.TextMessage;
+import com.riversoft.weixin.qy.request.QyTextRequest;
 import org.apache.commons.io.IOUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.ServletException;
@@ -22,7 +23,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.net.URLDecoder;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
@@ -38,20 +38,19 @@ public class ToncentServlet extends HttpServlet {
     String sToken = "8jRvcjG";
     String sCorpID = "wxf4774c756a351ff8";
     String sEncodingAESKey = "N1XM5YHIRS8Tcwsd8ctObavzqiruvxfT4WwotHhXGfQ";
-    private static Logger logger = LoggerFactory.getLogger(ToncentServlet.class);
+//    private static Logger logger = LoggerFactory.getLogger(ToncentServlet.class);
 
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-
-        System.out.println("request = " + request);
-
         response.setContentType("text/html;charset=utf-8");
         //response.setCharacterEncoding("utf-8");
         request.setCharacterEncoding("utf-8");
 
+        System.out.println("=========================doGet");
+        System.out.println("request.getSession().getId() = " + request.getSession().getId());
         Map parameterMap = request.getParameterMap();
         Set keySet = parameterMap.keySet();
         for (Iterator iterator = keySet.iterator(); iterator.hasNext(); ) {
@@ -61,25 +60,24 @@ public class ToncentServlet extends HttpServlet {
             System.out.println(paramName + " = " + paramValue);
         }
 
-        System.out.println("=========================doGet1");
+
+        //从请求中读取整个post数据
+        InputStream inputStream = request.getInputStream();
+        String postData = IOUtils.toString(inputStream);
+
+        System.out.println("postData = " + postData);
 
 
         try {
             WXBizMsgCrypt wxcpt = new WXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID);
 
-            System.out.println("=========================doGet2");
 
             // 解析出url上的参数值如下：
-            //URLDecoder.decode(request.getParameter("echostr"),"utf-8");
-            String sVerifyMsgSig = URLDecoder.decode(request.getParameter("msg_signature"), "utf-8");
-            String sVerifyTimeStamp = URLDecoder.decode(request.getParameter("timestamp"), "utf-8");
-            String sVerifyNonce = URLDecoder.decode(request.getParameter("nonce"), "utf-8");
-            String sVerifyEchoStr = URLDecoder.decode(request.getParameter("echostr"), "utf-8");
-
-            System.out.println("sVerifyMsgSig = " + sVerifyMsgSig);
-            System.out.println("sVerifyTimeStamp = " + sVerifyTimeStamp);
-            System.out.println("sVerifyNonce = " + sVerifyNonce);
-            System.out.println("sVerifyEchoStr = " + sVerifyEchoStr);
+            //request.getParameter("echostr"),"utf-8");
+            String sVerifyMsgSig = request.getParameter("msg_signature");
+            String sVerifyTimeStamp = request.getParameter("timestamp");
+            String sVerifyNonce = request.getParameter("nonce");
+            String sVerifyEchoStr = request.getParameter("echostr");
 
             PrintWriter out = response.getWriter();
             String sEchoStr; //需要返回的明文
@@ -99,56 +97,69 @@ public class ToncentServlet extends HttpServlet {
         }
     }
 
-    private String wapperMessage(XmlMessageHeader xmlRequest) throws JsonProcessingException {
+    private XmlMessageHeader wapperMessage(XmlMessageHeader xmlRequest) throws JsonProcessingException {
 
-        logger.info(xmlRequest.getFromUser());
-        logger.info(xmlRequest.getToUser());
-        logger.info(xmlRequest.getMsgType().name());
-        logger.info(xmlRequest.getCreateTime() + "");
+        System.out.println(xmlRequest.getFromUser());
+        System.out.println(xmlRequest.getToUser());
+        System.out.println(xmlRequest.getMsgType().name());
+        System.out.println(xmlRequest.getCreateTime() + "");
+
+        if (MsgType.event.equals(xmlRequest.getMsgType()) && xmlRequest instanceof ClickEvent) {
+            ClickEvent clickEvent = (ClickEvent)xmlRequest;
+            String eventKey = clickEvent.getEventKey();
+            if ("task".equals(eventKey)) {
+
+            }
+        }
         TextXmlMessage textXmlMessage = new TextXmlMessage();
         textXmlMessage.setFromUser(xmlRequest.getToUser());
         textXmlMessage.setToUser(xmlRequest.getFromUser());
         textXmlMessage.setCreateTime(new Date());
         textXmlMessage.setContent("有趣的测试");
 
-        return XmlObjectMapper.defaultMapper().toXml(textXmlMessage);
+        return textXmlMessage;
 //        xmlRequest.
 
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String signature = URLDecoder.decode(request.getParameter("msg_signature"), "utf-8");
-        String timestamp = URLDecoder.decode(request.getParameter("timestamp"), "utf-8");
-        String nonce = URLDecoder.decode(request.getParameter("nonce"), "utf-8");
-        String echostr = URLDecoder.decode(request.getParameter("echostr"), "utf-8");
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        System.out.println("request.getSession().getId() = " + request.getSession().getId());
 
-        logger.info("msg_signature={}, nonce={}, timestamp={}, echostr={}", signature, nonce, timestamp, echostr);
-
-        AgentSetting agentSetting = DefaultSettings.defaultSettings().getAgentSetting();
-        String corpId = DefaultSettings.defaultSettings().getCorpSetting().getCorpId();
-        String result = echostr;
 
         try {
-            MessageDecryption messageDecryption = new MessageDecryption(agentSetting.getToken(), agentSetting.getAesKey(), corpId);
+            String signature = request.getParameter("msg_signature");
+            String timestamp = request.getParameter("timestamp");
+            String nonce = request.getParameter("nonce");
+            String echostr = request.getParameter("echostr");
+
+
+            String result = "";
+
+
+            MessageDecryption messageDecryption = new MessageDecryption(sToken, sEncodingAESKey, sCorpID);
             if (!StringUtils.isEmpty(echostr)) {
-                String echo = messageDecryption.decryptEcho(signature, timestamp, nonce, echostr);
-                logger.info("消息签名验证成功.");
-                result =  echo;
+                result = messageDecryption.decryptEcho(signature, timestamp, nonce, echostr);
+                System.out.println("消息签名验证成功.");
             } else {
                 //从请求中读取整个post数据
                 InputStream inputStream = request.getInputStream();
-                String postData = IOUtils.toString(inputStream, "UTF-8");
+                String postData = IOUtils.toString(inputStream);
 
                 XmlMessageHeader xmlRequest = QyXmlMessages.fromXml(messageDecryption.decrypt(signature, timestamp, nonce, postData));
-                result = wapperMessage(xmlRequest);
+                XmlMessageHeader textXmlMessage = wapperMessage(xmlRequest);
+                String xml = XmlObjectMapper.defaultMapper().toXml(textXmlMessage);
+
+                result = messageDecryption.encrypt(xml, String.valueOf(textXmlMessage.getCreateTime().getTime() / 1000), nonce);
 
             }
-        } catch (Exception e) {
-            logger.error("callback failed.", e);
-        }
 
-        response.getWriter().write(result);
-        response.getWriter().close();
+            response.getWriter().write(result);
+            response.getWriter().close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
